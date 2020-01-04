f0c476479cb7fc36064ceb26df0464c82238cb98
valdir-ti <valdir.ti@gmail.com>
2020-01-04 00:29:59 -0300

    Ajustando para servir os arquivos com express

---
+++ index.js
@@ -0,0 +1,30 @@
+const PORT    = 3333;
+const express = require('express');
+const app     = express();
+const path    = require('path');
+const router  = express.Router();
+
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+router.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname+'/dist/about.html'));
+});
+
+router.get('/work', function(req, res){
+  res.sendFile(path.join(__dirname+'/dist/work.html'));
+});
+
+router.get('/contact', function(req, res){
+  res.sendFile(path.join(__dirname+'/dist/contact.html'));
+});
+
+app.use('/css', express.static(__dirname+'/dist/css'));
+app.use('/js', express.static(__dirname+'/dist/js'));
+app.use('/img', express.static(__dirname+'/dist/img'));
+
+app.use('/', router);
+app.listen(process.env.port || PORT);
+
+console.log(`App running at port ${PORT}`);
